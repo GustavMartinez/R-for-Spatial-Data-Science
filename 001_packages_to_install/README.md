757c88883430b6f6d64346e11c360655ca8fa6dc
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-07 15:44:19 -0300

    lista de packages para desarrollar analisis espaciales en r

---
+++ 001_packages_to_install/README.md
@@ -0,0 +1,7 @@
+## 🧠 Packages to install in R for Spatial Data Science
+- Lista con los principales paquetes para instalar y desarrollar proyectos en Spatial Data Science
+
+## ▶️ Ejecución
+Ejecutar el archivo principal:
+
+r packages_to_install.R
---
+++ 001_packages_to_install/packages_to_install.R
@@ -0,0 +1,38 @@
+# packages for vector, raster and spatial analysis
+
+install.packages("sf")
+install.packages("terra")
+install.packages("spatstat")
+install.packages("spdep")
+install.packages("stars")
+install.packages("gstat")
+install.packages("geosphere")
+install.packages("exactextractr")
+
+# packages for maps and visualization:
+
+install.packages("tmap")
+install.packages("leaflet")
+install.packages("mapview")
+install.packages("ggspatial")
+install.packages("rasterVis")
+
+# Packages for data science and spacial
+
+install.packages("tidyverse")
+install.packages("janitor")
+
+# Packages for conectivity and format
+
+install.packages("geojsonio")
+install.packages("osmdata")
+install.packages("DBI")
+install.packages("RPostgres")
+install.packages("curl")
+install.packages("httr2")
+
+# Optional packages
+
+install.packages("rgdal")
+install.packages("rgeos")
+install.packages("raster")
