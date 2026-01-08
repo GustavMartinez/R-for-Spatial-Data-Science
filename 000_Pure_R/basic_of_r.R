e49a2c82df0e5185846154682dc63a8a01839c77
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-08 15:58:14 -0300

    Archivos sobre R y estadistica en R

---
+++ 000_Pure_R/basic_of_r.R
@@ -0,0 +1,71 @@
+# This is a comment
+
+x <- 2
+
+x <- 5
+
+y <- x + 2
+
+
+# Remove a variable:
+
+rm(y)
+
+
+### Logic ##
+
+x <- 3
+z <- 9
+
+x > z # bigger than
+x < z # less than
+x == y # equal to
+x != y # non equal to
+
+
+## Data types ##
+
+
+# numeric (number)
+
+a <- 8
+class(a) # check the data class
+is.numeric(a)
+
+
+# integer (number with no decimals)
+
+
+ai <- as.integer(a)
+
+# Mathematical operations #
+
+# addition >>         +
+# Subtraction >>      -
+# Multiplication >>   *
+# Division >>         /
+# Exponentiation >>   ^
+# Modulo >>           %%
+
+
+# character (letters)
+
+z <- "lobster"
+t <- c("lobster")
+
+
+### VECTORS ###
+
+V1 <- c(1,2,3)
+v1
+
+
+
+
+
+
+
+
+
+
+
