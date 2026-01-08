9333b547a64304f872d9197d4a44234c5d5e94b0
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-08 15:59:33 -0300

    manipulacion de vectores en R - procesos básicos

---
+++ 000_Pure_R/basic_of_vectors.R
@@ -0,0 +1,13 @@
+# Crear un vector
+v1 <- c(1,2,3,4,5)
+
+# nombrar los elementos del vector
+names(v1) <- c("a", "b", "c", "d", "e")
+
+# nombrar los elementos de un vector, con el vector de nombres
+
+v2 <- c(5,6,7)
+vector_nombres <- c('z', 'y', 'x')
+
+names(v2) <- vector_nombres
+v2
