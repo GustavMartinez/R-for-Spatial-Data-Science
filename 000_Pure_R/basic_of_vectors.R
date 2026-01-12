0eff788d3f4a53bc8e356cfb25b6f7f265239393
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-12 15:08:48 -0300

    operaciones con vectores en R

--- 000_Pure_R/basic_of_vectors.R
+++ 000_Pure_R/basic_of_vectors.R
@@ -11,3 +11,19 @@ vector_nombres <- c('z', 'y', 'x')
 
 names(v2) <- vector_nombres
 v2
+
+# OPERACIONES CON VECTORES #
+
+# Definición de los vectores
+vector_1 <- c(2,4,6,8,10)
+vector_2 <- c(1,3,5,7,9)
+
+# Suma de los elementos entre vectores
+sum_vec <- vector_1 + vector_2
+sum_vec
+
+#Suma total entre todos los elementos de los dos vectores
+total_vect_1 <- sum(vector_1)
+total_vect_2 <- sum(vector_2)
+total_vectores <- total_vect_1 + total_vect_2
+total_vectores
