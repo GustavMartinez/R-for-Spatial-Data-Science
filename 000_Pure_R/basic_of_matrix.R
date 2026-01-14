2902a5c7d25ff835c58429332667b0ffe4f22e11
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-14 11:35:06 -0300

    basic of matrix in R

---
+++ 000_Pure_R/basic_of_matrix.R
@@ -0,0 +1,8 @@
+## matriz de 3 columnas y 3 lineas, con números de 1 a 9, aumentando en la linea
+a <- matrix(1:9, byrow=TRUE, nrow=3)
+a
+
+
+## matriz de 3 columnas, y 4 filas, con números de 1 a 12, aumentando en la columna
+b <- matrix(1:12, byrow = FALSE, nrow=4)
+b
