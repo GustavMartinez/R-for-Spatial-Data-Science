5f44932aa7a8972d0d5a1201cebd1709e523f496
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-14 15:53:54 -0300

    creación de matriz de vectores

--- 000_Pure_R/004_basic_of_matrix.R
+++ 000_Pure_R/004_basic_of_matrix.R
@@ -6,3 +6,17 @@ a
 ## matriz de 3 columnas, y 4 filas, con números de 1 a 12, aumentando en la columna
 b <- matrix(1:12, byrow = FALSE, nrow=4)
 b
+
+
+## matriz de tres vectores
+
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Crear un vector con todos los datos
+box_office <- c(new_hope, empire_strikes, return_jedi)
+
+# Construir una matriz
+sw_matrix <- matrix(box_office, nrow=3, byrow=TRUE)
+sw_matrix
